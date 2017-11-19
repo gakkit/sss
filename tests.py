7f418ad8ed27ab84afcd8e582b1942ab73533ae4
Asazhou <571061858@qq.com>
2017-11-19 18:14:11 +0800

    改了tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,1 @@
-from django.test import TestCase
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-browser = webdriver.Firefox()
-
-browser.get('http://www.yahoo.com')
-assert 'Yahoo' in browser.title
-
-elem = browser.find_element_by_name('p')  # Find the search box
-elem.send_keys('seleniumhq' + Keys.RETURN)
-
-browser.quit()
+print('hello world')
